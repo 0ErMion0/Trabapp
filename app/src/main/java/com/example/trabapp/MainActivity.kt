de32654db12998b6579a8e82d74d367d440077e1
Ermion <ermion0357@gmail.com>
2023-07-22 17:41:27 +0900

    Squashed commit of the following:
    commit c3e75e3efaed30747b397043ae966ac00a1a6fb3
    Author: Ermion <ermion0357@gmail.com>
    Date:   Sat Jul 22 17:33:17 2023 +0900
    
        button code prac
    
    commit 90611eb8342e1c9a95acbe7a7da6b7088f60cb4b
    Author: Ermion <ermion0357@gmail.com>
    Date:   Sat Jul 22 17:28:42 2023 +0900
    
        button add prac

--- app/src/main/java/com/example/trabapp/MainActivity.kt
+++ app/src/main/java/com/example/trabapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.trabapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    
+    lateinit var button1 : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        // 버튼 클릭 이벤트 작성
     }
 }
