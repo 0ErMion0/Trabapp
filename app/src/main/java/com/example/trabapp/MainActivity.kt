5fd968c159400e91440b9b3504e9d37f92e1ba01
Ermion <ermion0357@gmail.com>
2023-07-22 22:24:37 +0900

    develop: Google map add

--- app/src/main/java/com/example/trabapp/MainActivity.kt
+++ app/src/main/java/com/example/trabapp/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.trabapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+
 
 class MainActivity : AppCompatActivity() {
-    
-    lateinit var button1 : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
-        // 버튼 클릭 이벤트 작성
+
+        // Initialize fragment
+        val fragment = MapFragment()
+
+        // Open fragment
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame_layout, fragment)
+            .commit()
     }
 }
