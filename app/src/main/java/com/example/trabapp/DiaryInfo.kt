33fe553981a52a14893f95dd20ad3a6f788de04f
Ermion <ermion0357@gmail.com>
2023-07-24 15:31:36 +0900

    develop: add activities - 추억기록 - 내 정보 - 내 추억 - 추억 상세 - 일지 상세 - 내 일정

---
+++ app/src/main/java/com/example/trabapp/DiaryInfo.kt
@@ -0,0 +1,11 @@
+package com.example.trabapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DiaryInfo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diary_info)
+    }
+}
